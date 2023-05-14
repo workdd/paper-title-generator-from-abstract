f07e2b47b88304b7d3794dfbf6ed726f7ed311a9
manchann <chl8273@naver.com>
2023-05-14 21:31:01 +0900

    모델 로드 및 추론 람다 코드 작성 완료

--- app.py
+++ app.py
@@ -3,15 +3,10 @@ import json
 
 model = SimpleT5()
 model.from_pretrained("t5", "t5-base")
-model.load_model(r"outputs/simplet5-epoch-5-train-loss-1.3866-val-loss-1.7207", use_gpu=False)
-
-# generate
-model.predict("summarize:  some text you want to test it on")
+model.load_model(r"T5-epoch1", use_gpu=False)
 
 
 def handler(event, context):
-    sample_abstracts = test_df.sample(10)
-
     abstract = event['abstract']
 
     summary = model.predict(abstract)
