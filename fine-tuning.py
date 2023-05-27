9dedd0cdba2a5bd3a81fa9f48370a0c2b480dda3
manchann <chl8273@naver.com>
2023-05-27 16:26:11 +0900

    change batch size to 32

--- fine-tuning.py
+++ fine-tuning.py
@@ -96,6 +96,6 @@ model.train(train_df=train_df,
             source_max_token_len=512,
             target_max_token_len=128,
             max_epochs=1,
-            batch_size=16,
+            batch_size=32,
             use_gpu=True,
             dataloader_num_workers=32)
