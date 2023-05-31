fae04cf642fd29b3ced86177cbbb86b2ee87fbe6
manchann <chl8273@naver.com>
2023-05-31 16:52:41 +0900

    rm unusage commnad

--- app.py
+++ app.py
@@ -5,25 +5,18 @@ from simplet5 import SimpleT5
 import os
 os.environ['TRANSFORMERS_CACHE'] = '/tmp/cache/'
 
-json.dumps(parse_qs("a=1&b=2"))
-
-
 model = SimpleT5()
 model_name = os.environ['MODEL_NAME']
 model.load_model("t5", f"/var/task/{model_name}", use_gpu=False)
 
-
 def handler(event, context):
-    print(event)
     e = event['body']
     e = json.dumps(parse_qs(e))
     e = json.loads(e)
-    print(e)
     abstract = e['abstract'][0]
     abstract = "summarize: " + abstract
 
     summary = model.predict(abstract)[0]
-    print(summary)
     result = {'summary': summary}
     return {
         'statusCode': 200,
